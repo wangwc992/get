a69451633e53705307b35f0cad0f80b92cc8a1ec
wishfyc <wishfyc@136.com>
2022-07-31 09:51:29 +0800

    第一次提交

---
+++ src/main/java/com/atguigu/FastTest.java
@@ -0,0 +1,14 @@
+package com.atguigu;
+
+import sun.applet.Main;
+
+/**
+ * @author wishfyc
+ * @create 2022-07-31 9:50
+ * @description
+ */
+public class FastTest {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
