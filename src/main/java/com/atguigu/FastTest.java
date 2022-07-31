30bf2fdb9cf0750529f5166e2f760d6f28239d23
wishfyc <wishfyc@136.com>
2022-07-31 10:29:31 +0800

    本地 111

--- src/main/java/com/atguigu/FastTest.java
+++ src/main/java/com/atguigu/FastTest.java
@@ -11,5 +11,6 @@ public class FastTest {
     public static void main(String[] args) {
         System.out.println("hello hot-fix");
         System.out.println("hello hot-fixTest1");
+        System.out.println("重新添加");
     }
 }
