29483f02f88708a04dd848e503459e2bcb3cd724
wishfyc <wishfyc@136.com>
2022-07-31 09:56:32 +0800

    第一次提交

--- src/main/java/com/atguigu/FastTest.java
+++ src/main/java/com/atguigu/FastTest.java
@@ -10,5 +10,7 @@ import sun.applet.Main;
 public class FastTest {
     public static void main(String[] args) {
         System.out.println("args = " + args);
+        System.out.println("args = " + args);
+        System.out.println("args = " + args);
     }
 }
