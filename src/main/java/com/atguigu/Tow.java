8b2104d0787deb9d85293d90dd2b31a51b985621
wishfyc <wishfyc@136.com>
2022-07-31 10:49:13 +0800

    本地 3333

--- src/main/java/com/atguigu/Tow.java
+++ src/main/java/com/atguigu/Tow.java
@@ -7,13 +7,7 @@ package com.atguigu;
  */
 public class Tow {
     public static void main(String[] args) {
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");  System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");  System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
+        System.out.println("333");
+
     }
 }
